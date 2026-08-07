8f4a9a2d0bf1d0bd8fdd0ea7c7b1ce93d83f164d
ratolon <miceta@cnb.csic.es>
2026-08-07 11:47:42 +0000

    Add versions, python deps

--- simple/constants.py
+++ simple/constants.py
@@ -1,8 +1,9 @@
 # **************************************************************************
 # *
-# * Authors:     Grigory Sharov (gsharov@mrc-lmb.cam.ac.uk)
+# * Authors:     Mikel Iceta (miceta@cnb.csic.es)
 # *
-# * MRC Laboratory of Molecular Biology (MRC-LMB)
+# * Natl Center for Biotechnology CNB-CSIC
+# * NY Structural Biology Center (NYSBC)
 # *
 # * This program is free software; you can redistribute it and/or modify
 # * it under the terms of the GNU General Public License as published by
@@ -24,4 +25,63 @@
 # *
 # **************************************************************************
 
-SIMPLE_HOME = 'SIMPLE-3.0'
+SIMPLE_HOME = 'SIMPLE_HOME'
+
+# Supported versions
+V3_0_0 = '3.0.0'
+V3_1_BETA = '3.1-beta'
+V4_0_0 = '4.0.0'
+
+VERSIONS = [V3_0_0, V3_1_BETA, V4_0_0]
+SIMPLE_DEFAULT_VER_NUM = V4_0_0
+
+# Home folder default for selected version
+DEFAULT_HOME = f'SIMPLE-{SIMPLE_DEFAULT_VER_NUM}'
+
+DEFAULT_ENV_NAME = f'simple-{SIMPLE_DEFAULT_VER_NUM}'
+DEFAULT_ACTIVATION_CMD = 'conda activate ' + DEFAULT_ENV_NAME
+SIMPLE_ENV_ACTIVATION = 'SIMPLE_ENV_ACTIVATION'
+
+# Git source per supported version
+SIMPLE_GIT_URL = 'https://github.com/hael/SIMPLE.git'
+SIMPLE_GIT_REFS = {
+	V3_0_0: 'v3.0.0',
+	V3_1_BETA: 'v3.1-beta',
+	V4_0_0: '3318d60',
+}
+
+# Build/runtime dependencies based on SIMPLE README and compile_conda.sh
+SIMPLE_CONDA_PACKAGES = [
+	'python=3.10',
+	'cmake',
+	'gcc=15.2.0',
+	'gxx=15.2.0',
+	'gfortran=15.2.0',
+	'fftw',
+	'libtiff',
+	'jbig',
+	'jpeg',
+	'openblas',
+	'lapack',
+	'arpack',
+	'libcurl',
+	'pip',
+]
+
+# Python tools used by SIMPLE scripts/NICE utilities
+SIMPLE_PIP_PACKAGES = [
+	'numpy',
+	'mrcfile',
+	'matplotlib',
+	'pandas',
+	'pillow',
+	'reportlab',
+	'django==5.2.5',
+	'pytailwindcss==0.2.0',
+	'dirsync==2.2.6',
+	'gunicorn==23.0.0',
+]
+
+SIMPLE_PIP_PACKAGES_LINUX = [
+	'pysqlite3-binary==0.5.4',
+]
