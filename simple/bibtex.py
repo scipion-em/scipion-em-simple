68e04f554b37c5364fc02cf8faff5960cb9ed1c7
ratolon <miceta@cnb.csic.es>
2026-08-07 22:04:21 +0000

    Update bib

--- simple/bibtex.py
+++ simple/bibtex.py
@@ -79,4 +79,34 @@
   Url                      = {http://dx.doi.org/10.1016/j.str.2013.07.002}
 }
 
+@article{Van_2025_abinitio3d,
+author = "Van, Cong T. S. and Reboul, Cyril F. and Caesar, Joseph J. E. and Meana-Pa{\~{n}}eda, Rub{\'{e}}n and Lountos, George T. and Deme, Justin C. and Bryant, Owain J. and Johnson, Steven and Piczak, Claire T. and Valkov, Eugene and Lea, Susan M. and Elmlund, Hans",
+title = "{Probabilistic single-particle cryo-EM {\it ab initio} 3D reconstruction in {\it SIMPLE}}",
+journal = "Acta Crystallographica Section D",
+year = "2025",
+volume = "81",
+number = "8",
+pages = "396--409",
+month = "Aug",
+doi = {10.1107/S2059798325005686},
+url = {https://doi.org/10.1107/S2059798325005686},
+abstract = {Three-dimensional (3D) structure determination by single-particle analysis of cryo-electron microscopy (cryo-EM) images requires {\it ab initio} 3D reconstruction of density volume(s) from 2D images (particles). This large-scale inverse problem requires the determination of many million degrees of freedom from extremely noisy experimental measurements. Here, we introduce a new approach to probabilistic multi-volume {\it ab initio} 3D reconstruction for simultaneous estimation of the relative particle 3D orientations and partitioning of the particles into groups with distinct structural states. To account for further structural variability within the discrete state groups, due to for example regional disorder, flexibility or partial occupancy of associating ligands, we introduce a new method for adaptive non-uniform regularization based on iterated conditional modes (ICMs). Our ICM regularization approach can be viewed as a spatially varying real-space prior that optimizes the connectivity of the reconstructed density map(s). Our method is designed to run in real time as the microscope collects the data, which puts significant constraints on algorithm scalability and flexibility with regard to how new particles are incorporated. We describe the probabilistic optimization and non-uniform regularization theory in detail. Finally, we provide numerous benchmarking examples, both on publicly available standard test data sets and on data sets acquired at our cryo-EM facility at the National Cancer Institute, National Institutes of Health. The implementation of our new multi-volume {\it ab initio} 3D reconstruction approach is part of the {\it SIMPLE} software suite, which is provided open source at https://github.com/hael/SIMPLE.},
+keywords = {cryo-EM, single particle, reconstruction, probabilistic, heterogeneity},
+}
+
+@article{Van_2026_polar,
+author = "Van, Cong T. S. and Reboul, Cyril F. and Caesar, Joseph J. E. and Meana-Pa{\~{n}}eda, Rub{\'{e}}n and Elmlund, Hans",
+title = "{A polar Fourier geometric approach to volume-free single-particle 3D reconstruction}",
+journal = "IUCrJ",
+year = "2026",
+volume = "13",
+number = "4",
+pages = "354--363",
+month = "Jul",
+doi = {10.1107/S2052252526003611},
+url = {https://doi.org/10.1107/S2052252526003611},
+abstract = {We introduce a compact mathematical formulation for the inverse single-particle 3D reconstruction problem, a high-dimensional inverse problem in which millions of parameters are estimated from extremely noisy experimental measurements. Given a collection of noisy 2D projection images (particles) of an unknown 3D charge-density distribution, the objective is to infer the unknown particle orientations and thereby enable {\it ab initio} 3D reconstruction via tomographic methods. We develop a method for generating regularized reprojections directly from the noisy particles that does not rely on explicit 3D density reconstruction. Instead, we recast the {\it ab initio} orientation-recovery problem in polar Fourier coordinates through discretization of the rotation group SO(3). The directions of projection are mapped onto slices intersecting the origin of the 3D Fourier transform. The rotations in the plane normal to a projection direction are mapped onto radial lines in the 2D Fourier transforms of the particles. An optimization procedure jointly estimates particle projection directions, in-plane rotation angles and rotational origin offsets. Regularized reprojections are computed by averaging along lines in the polar Fourier representation, exploiting data redundancy to suppress noise and improve stability. We present the mathematical framework in detail and provide initial benchmarks demonstrating the performance and robustness of the approach.},
+keywords = {single-particle 3D reconstruction, polar Fourier coordinates, orientation-recovery problem},
+}
+
 """
