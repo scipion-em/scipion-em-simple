32fff1d88b872740951389198c5635b5602b3728
ratolon <miceta@cnb.csic.es>
2026-08-07 21:28:18 +0000

    Deprecate Unblur

--- simple/protocols/__init__.py
+++ simple/protocols/__init__.py
@@ -24,7 +24,6 @@
 # *
 # **************************************************************************
 
-from .protocol_preprocess import ProtUnblurSimple
 from .protocol_cluster2D import ProtCluster2D
 from .protocol_init3D import ProtInit3D
 from .protocol_symmetry import ProtSym
